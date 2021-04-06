28c8c47ff35605277bc88c99baca0bd02f1cf022
dunjajazavac <martinovicdunja20@gmail.com>
2021-04-06 11:44:09 +0200

    inicijalni commit

---
+++ podsistemProdaje/src/test/java/com/poslovnaInformatika/podsistemProdaje/PodsistemProdajeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poslovnaInformatika.podsistemProdaje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodsistemProdajeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
