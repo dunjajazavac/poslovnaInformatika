05347f72a28334193558ad4d0622c2e35cb9df6a
Elena Krunic <elenakrunic@gmail.com>
2021-08-30 11:23:19 +0200

    dodati jarovi

--- podsistemProdaje/src/main/java/com/poslovnaInformatika/podsistemProdaje/model/PoslovnaGodina.java
+++ podsistemProdaje/src/main/java/com/poslovnaInformatika/podsistemProdaje/model/PoslovnaGodina.java
@@ -15,9 +15,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.sun.istack.internal.NotNull;
 
 
 
