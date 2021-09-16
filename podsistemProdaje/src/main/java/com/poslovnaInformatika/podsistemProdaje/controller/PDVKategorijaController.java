1ee5077fbf25b1e6634b9073f4b01d6ea1e803dd
Elena Krunic <elenakrunic@gmail.com>
2021-09-16 08:10:15 +0200

    dodala sorting metode u repozitorijumu i kontroleru za pdvkategoriju

--- podsistemProdaje/src/main/java/com/poslovnaInformatika/podsistemProdaje/controller/PDVKategorijaController.java
+++ podsistemProdaje/src/main/java/com/poslovnaInformatika/podsistemProdaje/controller/PDVKategorijaController.java
@@ -2,9 +2,16 @@
 package com.poslovnaInformatika.podsistemProdaje.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -15,10 +22,12 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.poslovnaInformatika.podsistemProdaje.dto.PDVKategorijaDTO;
 import com.poslovnaInformatika.podsistemProdaje.model.PDVKategorija;
+import com.poslovnaInformatika.podsistemProdaje.repository.PDVKategorijaRepository;
 import com.poslovnaInformatika.podsistemProdaje.service.PDVKategorijaService;
 
 @CrossOrigin
@@ -29,6 +38,9 @@ public class PDVKategorijaController {
 
 	@Autowired
 	private PDVKategorijaService pdvKategorijaService; 
+	
+	@Autowired
+	private PDVKategorijaRepository pdvKategorijaRepository; 
 	
 	@RequestMapping(value="/all", method = RequestMethod.GET)
 	public ResponseEntity<List<PDVKategorijaDTO>> getAllPDVKategorija(){
@@ -89,5 +101,63 @@ public class PDVKategorijaController {
 		}
 	}
 	
+	@RequestMapping(value="/allSorted", method = RequestMethod.GET)
+	public ResponseEntity<Map<String, Object>> getAllPdvCategories(
+			@RequestParam(required = false) String name, 
+			@RequestParam(defaultValue="0") int page, 
+			@RequestParam(defaultValue="3") int size,
+			@RequestParam(defaultValue="id, desc") String[] sort) {
+		
+		try {
+			 List<Order> orders = new ArrayList<Order>();
+
+		      if (sort[0].contains(",")) {
+		        // will sort more than 2 fields
+		        // sortOrder="field, direction"
+		        for (String sortOrder : sort) {
+		          String[] _sort = sortOrder.split(",");
+		          orders.add(new Order(getSortDirection(_sort[1]), _sort[0]));
+		        }
+		      } else {
+		        // sort=[field, direction]
+		        orders.add(new Order(getSortDirection(sort[1]), sort[0]));
+		      }
+		      
+		      List<PDVKategorija> pdvKategorije = new ArrayList<>();
+		      Pageable paging = PageRequest.of(page, size);
+		      
+		      Page<PDVKategorija> pagePdvKategorije; 
+		      
+		      if(name == null) 
+		    	  pagePdvKategorije = pdvKategorijaRepository.findAll(paging);
+		      else 
+		    	  pagePdvKategorije = pdvKategorijaRepository.findByNazivKategorije(name, paging);
+		      
+		      pdvKategorije = pagePdvKategorije.getContent();
+		      
+		      Map<String, Object> response = new HashMap<>();
+		      response.put("pdvKategorije", pdvKategorije);
+		      response.put("currentPage", pagePdvKategorije.getNumber());
+		      response.put("totalItems", pagePdvKategorije.getTotalElements());
+		      response.put("totalPages", pagePdvKategorije.getTotalPages());
+		      
+		      return new ResponseEntity<>(response, HttpStatus.OK);
+		} catch(Exception e ) {
+			e.printStackTrace();
+		}
+		
+		return null; 
+	}
+	
+	//helper method 
+		private Sort.Direction getSortDirection(String direction) {
+		    if (direction.equals("asc")) {
+		      return Sort.Direction.ASC;
+		    } else if (direction.equals("desc")) {
+		      return Sort.Direction.DESC;
+		    }
+
+		    return Sort.Direction.ASC;
+		  }
 	
 }
--- podsistemProdaje/src/main/java/com/poslovnaInformatika/podsistemProdaje/repository/PDVKategorijaRepository.java
+++ podsistemProdaje/src/main/java/com/poslovnaInformatika/podsistemProdaje/repository/PDVKategorijaRepository.java
@@ -1,5 +1,10 @@
 package com.poslovnaInformatika.podsistemProdaje.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +13,7 @@ import com.poslovnaInformatika.podsistemProdaje.model.PDVKategorija;
 @Repository
 public interface PDVKategorijaRepository extends JpaRepository<PDVKategorija, Long> {
 
+	Page<PDVKategorija> findAll(Pageable pageable);
+	Page<PDVKategorija> findByNazivKategorije(String name, Pageable page);
+	List<PDVKategorija> findByNazivKategorije(String name, Sort sort);
 }
